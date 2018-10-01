68e2d9d4733a752d83e2e4c1cdee4899e23fc325
Yingge He <heyingge96@gmail.com>
2018-10-01 05:53:15 -0700

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Yingge He on 10/1/18.
+//  Copyright © 2018 Yingge He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
